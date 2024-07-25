256bc60e7367175ca36230d8f3be9a1837dd34a5
sagar wankhede <swankhede639@gmail.com>
2024-07-25 14:52:10 +0530

    add print tag and print kind and lastModified and menu

--- src/component/GoogleFonts.js
+++ src/component/GoogleFonts.js
@@ -24,18 +24,22 @@ function GoogleFonts(props) {
         </div>
         {googlefonts.map((items,id) => {
           return (
-            <div key={id} className="card col-md-3 mx-2 my-2" style={{width: "350px"}} >
+            <div key={id} className="card col-md-3 mx-2 my-2 p-0" style={{width: "350px"}} >
               {/* <img src="..." className="card-img-top" alt="..." /> */}
               <div className="card-body">
-                <p className="card-title">Category:{items.category}</p>
-                <p className="card-text">family:{items.family}</p>
+                <p className="card-title">Category: {items.category}</p>
+                <p className="card-text">family: {items.family}</p>
                 <div className="d-flex justify-content-between flex-column">
-                <span>italic:</span><a href={items.files.italic}>
+                <span className="d-inline">italic:</span><a className="d-inline" href={items.files.italic}>
                   {items.files.italic === undefined ? 'not available' : items.files.italic }              
                 </a>
                 <span>regular:</span><a href={items.files.regular}> 
                 {items.files.regular === undefined ? 'not available' : items.files.regular } 
                 </a>
+                <p className="my-0">Kind: {items.kind}</p>
+                <p className="my-0">lastModified: {items.lastModified}</p>
+                <span>Menu:</span> <a href={items.menu}>{items.menu}</a>
+            
                 </div>
               </div>
             </div>
